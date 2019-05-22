e5457fe3f9b7eb9109e0e66a9ef22802295b2d2b
zerol <zerolfx0@gmail.com>
2019-05-22 20:22:30 +0800

    change classifier

--- solution/go.py
+++ solution/go.py
@@ -1,12 +1,14 @@
-from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_validate
+import warnings
 from utils import *
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 features1 = np.mat(pd.read_pickle('features1.pickle'))
 features2 = np.mat(pd.read_pickle('features2.pickle'))
-features = np.hstack((features1, features2))
-print(features.shape)
+features = np.hstack([features1, features2])
 
 
-res = cross_validate(GaussianNB(), features, data_y, cv=10, return_train_score=True)
+res = cross_validate(RandomForestClassifier(), features, data_y, cv=10, return_train_score=True)
 print(np.average(res['train_score']))
